65ae6ea403e6ae1d569d0e1f246fce4603ed7183
Valdeschultz <Vald0202@hotmail.com>
2023-10-16 19:12:21 +1100

    Firstcommit

---
+++ App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUpScreen from './Components/Signupscreen';
+import LoginScreen from './Components/Loginscreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignUp">
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
